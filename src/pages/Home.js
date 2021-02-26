647f766d82a0b9ce788851399a12911a25abd97e
Bhavjot Singh <bhavjot794@gmail.com>
2021-02-27 01:05:48 +0530

    Github api working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,57 @@
-import React from "react"
+import React, { useState, useContext } from "react";
+import Axios from "axios";
 
+import {
+  Row,
+  Container,
+  Col,
+  Input,
+  Button,
+  InputGroup,
+  InputGroupAddon
+} from "reactstrap";
+
+import UserCard from "../components/UserCard";
+import Repos from "../components/Repos";
+import { Redirect } from "react-router-dom";
+import { UserContext } from "../Context/UserContext";
+import { toast } from "react-toastify";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-};
 
-export default Home;
+    const context = useContext(UserContext)
+    const [query,setQuery] = useState('')
+    const [user,setUser ] = useState(null)
+
+    const fetchDetails = async () => {
+         try {
+          const {data} = await Axios.get(`https://api.github.com/users/${query}`)
+          setUser(data)
+          console.log({data})
+         }
+         catch(error) {
+             toast("Not able to locate user",{type:"error"})
+         }
+    }
+return (
+    <Container>
+      <Row className=" mt-3">
+        <Col md="5">
+          <InputGroup>
+            <Input
+              type="text"
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+              placeholder="Please provide the username"
+            />
+            <InputGroupAddon addonType="append">
+              <Button onClick={fetchDetails}color="primary">Fetch User</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Col>
+        <Col md="7"></Col>
+      </Row>
+    </Container>
+  );
+}
+export default Home
